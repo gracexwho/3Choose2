50606e10d61c01ea1c2543bfeb122d130d424246
grace <grace.hu101@gmail.com>
2019-11-15 23:36:34 -0500

    Initialized project

---
+++ app/src/main/java/com/example/a3choose2/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.a3choose2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
